0ba54095b88f339d0c0f8ea1043776b6e3719a0a
Maria Sahno <sahnomyu@m2.ru>
2023-03-01 22:10:56 +0600

    init commit

---
+++ src/main/java/ru/yandex/practicum/webinars/Webinar9Application.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.webinars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webinar9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webinar9Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/practicum/webinars/Webinar9ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.webinars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webinar9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
