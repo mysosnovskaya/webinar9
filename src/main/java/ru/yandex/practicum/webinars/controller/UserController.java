8a9c8cbb0990a595b067ceb94b74adddd9a3ea64
PiFFFko <piwen.roman@yandex.ru>
2023-03-01 19:42:00 +0300

    feat: add admin user

--- src/main/java/ru/yandex/practicum/webinars/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/webinars/controller/UserController.java
@@ -9,12 +9,14 @@ import ru.yandex.practicum.webinars.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final List<User> users = new ArrayList<>();
-    private int idGenerator = 1;
+    private final List<User> users = new ArrayList<>(List.of((
+            new User(1,"admin","admin@yandex.ru","password"))));
+    private int idGenerator = 2;
 
     @PostMapping()
     public User register(@RequestBody User user) {
@@ -25,6 +27,6 @@ public class UserController {
 
     @GetMapping()
     public List<User> getAllUsers() {
-        return users;
+        return users.stream().filter(user -> user.getId() != 1).collect(Collectors.toList());
     }
 }
